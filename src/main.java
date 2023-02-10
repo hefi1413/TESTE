f669c7386b0e1f8c269c5dc56ea7c6e13641d115
hefi1413 <hefi1413@gmail.com>
2023-02-10 12:17:29 -0300

    inicio

--- src/main.java
+++ src/main.java
@@ -2,9 +2,7 @@ public class main {
 
     public static void main(String[] args) {
         System.out.println("Eu sou o seu primeiro programa.");
+        System.out.println("Eu sou o seu primeiro programa.");
     }
 
-    private void primeiroMetodo() {
-        System.out.println("Executando o primeiro metodo.");
-    }
 }
